4f20d8eca97e29792b94da1545f119d69b89019f
bluedream74 <137847214+bluedream74@users.noreply.github.com>
2024-01-14 10:46:44 +0900

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -148,8 +148,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT  = {
-'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+'ACCESS_TOKEN_LIFETIME': timedelta(minutes=int(os.getenv('ACCESS_TIME'))),
+'REFRESH_TOKEN_LIFETIME': timedelta(days=int(os.getenv('REFRESH_TIME'))),
 }
 
 CORS_ORIGIN_WHITELIST = [
